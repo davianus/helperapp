20a2c0580cdc98db6ae692eeb66a10d0ae588049
Vincenzo Sessa <captainsessa@hotmail.com>
2015-11-14 16:53:55 +0100

    Returns top 5 tags

--- backend/src/main/java/at/ac/tuwien/mase/backend/controllers/TagController.java
+++ backend/src/main/java/at/ac/tuwien/mase/backend/controllers/TagController.java
@@ -1,13 +1,19 @@
 package at.ac.tuwien.mase.backend.controllers;
 
+import at.ac.tuwien.mase.backend.models.Tag;
 import at.ac.tuwien.mase.backend.repositories.interfaces.ITagRepository;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by xvinci on 11/14/15.
@@ -23,7 +29,13 @@ public class TagController {
     public TagController(ITagRepository repo) {this.repository = repo;}
 
     @RequestMapping(method= RequestMethod.GET)
-    public void  test(@PathVariable("q") String query) {
-        logger.debug("Testlog");
+    public List<String>  getSimilarTags(@PathVariable("q") String query) {
+        logger.debug("Getting similar tags for tag "+query);
+
+        List<Tag> qresult = repository.findAll(new Sort(Sort.Direction.DESC, "count")).subList(0, 4);
+
+        List<String> result = qresult.stream().map(Tag::getName).collect(Collectors.toList());
+
+        return result;
     }
 }
