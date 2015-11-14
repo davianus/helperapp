951f04fd2468653218ccc3e41d5db94fb2bac143
David Schneiderbauer <dschneiderbauer@icloud.com>
2015-11-14 18:42:00 +0100

    registration; image

--- frontend/helperapp/www/js/app.js
+++ frontend/helperapp/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,85 +25,84 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
   .state('welcome', {
-      url: '/welcome',
-      templateUrl: 'templates/welcome.html',
-      controller: 'AppCtrl',
-      data: {
-        requireLogin: false
-      }
-    })
-    .state('registration', {
-      url: '/registration',
-      templateUrl: 'templates/registration.html',
-      controller: 'RegistrationCtrl',
-      data: {
-        requireLogin: false
-      }
-    })
+    url: '/welcome',
+    templateUrl: 'templates/welcome.html',
+    controller: 'AppCtrl',
+    data: {
+      requireLogin: false
+    }
+  })
+  .state('registration', {
+    url: '/registration',
+    templateUrl: 'templates/registration.html',
+    controller: 'RegistrationCtrl',
+    data: {
+      requireLogin: false
+    }
+  })
   .state('app', {
-      url: '/app',
-      abstract: true,
-      templateUrl: 'templates/menu.html',
-      controller: 'AppCtrl',
-      data: {
-        requireLogin: true // this property will apply to all children of 'app'
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl',
+    data: {
+      requireLogin: true // this property will apply to all children of 'app'
+    }
+  })
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
       }
-    })
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/search.html'
-        }
-      }
-    })
+    }
+  })
 
   .state('app.needs', {
-      url:'/needs',
+    url:'/needs',
     abstract: true,
     views: {
       'menuContent': {
         templateUrl: 'templates/needs.html'
       }
     }
-    })
-    .state('app.needs.byme',{
-      url:'/byme',
-      views: {
-        'byme-tab': {
-          templateUrl: 'templates/needs/byme.html',
-          controller: 'ByMeCtrl'
-        }
+  })
+  .state('app.needs.byme',{
+    url:'/byme',
+    views: {
+      'byme-tab': {
+        templateUrl: 'templates/needs/byme.html',
+        controller: 'ByMeCtrl'
       }
-    })
-    .state('app.needs.forme',{
-      url:'/forme',
-      views: {
-        'forme-tab': {
-          templateUrl: 'templates/needs/forme.html',
-          controller: 'ForMeCtrl'
-        }
+    }
+  })
+  .state('app.needs.forme',{
+    url:'/forme',
+    views: {
+      'forme-tab': {
+        templateUrl: 'templates/needs/forme.html',
+        controller: 'ForMeCtrl'
       }
-    })
-    .state('app.needs.todo',{
-      url:'/todo',
-      views: {
-        'todo-tab': {
-          templateUrl: 'templates/needs/todo.html',
-          controller: 'ToDoCtrl'
-        }
+    }
+  })
+  .state('app.needs.todo',{
+    url:'/todo',
+    views: {
+      'todo-tab': {
+        templateUrl: 'templates/needs/todo.html',
+        controller: 'ToDoCtrl'
       }
-    })
-
+    }
+  })
   .state('app.needDetail', {
     url: '/needDetail/:needId',
-      views: {
-        'menuContent': {
+    views: {
+      'menuContent': {
         templateUrl: 'templates/needDetail.html',
         controller: 'NeedDetailCtrl'
-        }
       }
-    });
+    }
+  });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/welcome');
 });
--- frontend/helperapp/www/js/controllers/registration.controller.js
+++ frontend/helperapp/www/js/controllers/registration.controller.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('RegistrationCtrl',function($scope, $ionicModal, $timeout, $state) {
+.controller('RegistrationCtrl',function($scope, $ionicModal, $timeout, $state, $cordovaCamera) {
 
   // Form data for the login modal
   $scope.loginData = {};
@@ -9,17 +9,23 @@ angular.module('starter.controllers')
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.loginModal = modal;
+  });
+
+  $ionicModal.fromTemplateUrl('templates/image-upload.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.uploadModal = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.loginModal.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    $scope.loginModal.show();
   };
 
   // Perform the login action when the user submits the login form
@@ -34,6 +40,48 @@ angular.module('starter.controllers')
       $state.go('app.needs.byme')
     }, 1000);
   };
+
+  $scope.closeUpload = function() {
+    $scope.uploadModal.hide();
+    $scope.photo = "";
+  };
+
+  $scope.upload = function() {
+    $scope.uploadModal.show();
+    initializeCamera();
+  };
+
+  $scope.doUpload = function() {
+    $scope.uploadModal.hide();
+  };
+
+  $scope.changeImage = function() {
+    initializeCamera();
+  };
+
+  var initializeCamera = function() {
+    document.addEventListener("deviceready", function () {
+      var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 100,
+        targetHeight: 100,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+        correctOrientation:true
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        $scope.photo = "data:image/jpeg;base64," + imageData;
+      }, function(err) {
+        // error
+      });
+    });
+  };
+
   $scope.doRegistration = function() {
     //TODO: registrate
     $timeout(function() {
