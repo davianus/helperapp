e6e6a89c7be1cc09867cd30e4218c0c57e13acb1
Vincenzo Sessa <captainsessa@hotmail.com>
2015-11-14 18:14:27 +0100

    Added findByName for tags

--- backend/src/main/java/at/ac/tuwien/mase/backend/repositories/interfaces/ITagRepository.java
+++ backend/src/main/java/at/ac/tuwien/mase/backend/repositories/interfaces/ITagRepository.java
@@ -2,9 +2,13 @@ package at.ac.tuwien.mase.backend.repositories.interfaces;
 
 import at.ac.tuwien.mase.backend.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by xvinci on 11/14/15.
  */
 public interface ITagRepository extends JpaRepository<Tag, String> {
+    @Query("SELECT t FROM Tag t WHERE t.name LIKE :name")
+    public Tag findByName(@Param("name") String name);
 }
