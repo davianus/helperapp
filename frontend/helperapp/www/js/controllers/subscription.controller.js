3bf64a092c769e9fb31ac450fbd7903725fb3bf7
David Schneiderbauer <dschneiderbauer@icloud.com>
2015-11-14 19:54:07 +0100

    create subscribe

--- frontend/helperapp/www/js/controllers/subscription.controller.js
+++ frontend/helperapp/www/js/controllers/subscription.controller.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('SubscriptionCtrl', function($scope) {
+.controller('SubscriptionCtrl', function($scope, $ionicModal, $timeout) {
   $scope.subscriptions = [
     {
       tags: [
@@ -15,4 +15,33 @@ angular.module('starter.controllers')
       ]
     }
   ];
+
+  $ionicModal.fromTemplateUrl('templates/new-subscription.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeSubscribe = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.subscribe = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doSubscribe = function() {
+    //console.log('Doing login', $scope.loginData);
+    //TODO: check password
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeNewSubscription();
+    }, 1000);
+  };
+
 });
