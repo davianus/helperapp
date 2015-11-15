161090bc3d4bba79f961b61b87cdc2aed30447c4
David Schneiderbauer <dschneiderbauer@icloud.com>
2015-11-15 12:12:59 +0100

    subscriptions

--- frontend/helperapp/www/js/controllers/subscription.controller.js
+++ frontend/helperapp/www/js/controllers/subscription.controller.js
@@ -1,6 +1,9 @@
 angular.module('starter.controllers')
 
-.controller('SubscriptionCtrl', function($scope, $ionicModal, $timeout, Subscription, Tag) {
+.controller('SubscriptionCtrl', function($scope, $ionicModal, $filter, Subscription, Tag) {
+
+  $scope.subscription = new Subscription();
+
   $scope.subscriptions = Subscription.query({
     user: 'test'
   });
@@ -23,14 +26,16 @@ angular.module('starter.controllers')
 
   // Perform the login action when the user submits the login form
   $scope.doSubscribe = function() {
-    //console.log('Doing login', $scope.loginData);
-    //TODO: check password
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeSubscribe();
-    }, 1000);
+    $scope.subscription.user = 'test';
+    $scope.subscription.start = $filter('date')($scope.subscription.startDate, 'yyyy-MM-dd');
+    delete $scope.subscription.startDate;
+    $scope.subscription.end = $filter('date')($scope.subscription.endDate, 'yyyy-MM-dd');
+    delete $scope.subscription.endDate;
+    Subscription.save($scope.subscription, function() {
+      $scope.subscriptions = Subscription.query({
+        user: 'test'
+      });
+    });
   };
 
   $scope.callbackMethod = function (query) {
