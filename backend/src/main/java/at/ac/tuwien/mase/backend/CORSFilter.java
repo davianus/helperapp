42e007770b409bc86254814307c27865882bd793
Michael Kaiser <michael.kaiser.at@gmail.com>
2016-01-06 09:03:36 +0100

    fixed corsfilter

--- backend/src/main/java/at/ac/tuwien/mase/backend/CORSFilter.java
+++ backend/src/main/java/at/ac/tuwien/mase/backend/CORSFilter.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.mase.backend;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -19,11 +20,16 @@ public class CORSFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-		filterChain.doFilter(servletRequest, servletResponse);
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			filterChain.doFilter(servletRequest, servletResponse);
+		}
 	}
 
 	@Override
